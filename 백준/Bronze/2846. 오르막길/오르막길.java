b39febb93bba57466ecc0c6a36617e01590bff1b
dasoLee <116339021+Losadee@users.noreply.github.com>
2022-11-17 14:44:24 +0900

    Update 오르막길.java

--- 백준/Bronze/2846. 오르막길/오르막길.java
+++ 백준/Bronze/2846. 오르막길/오르막길.java
@@ -13,24 +13,24 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 		
-		int start = 0;
+				int start = 0;
 		int max = 0;
 		
-		for(int i = 1; i < arr.length; i++) { 	
-			x1 = arr[start];					
+		for(int i = 1; i < arr.length; i++) { 		// while 조건문이 i-1부터라 시작을 1로 지정
+			x1 = arr[start];			// 출발점 x1에 저장 (arr[0] 부터 시작)
 			
-			while(arr[i-1] < arr[i]) {			
-				x2 = arr[i];					
+			while(arr[i-1] < arr[i]) {		// 오르막이면(다음 index의 값이 크면)
+				x2 = arr[i];			// x2에 값 저장하고
 				i++;
-				if(i == arr.length) break;		
+				if(i == arr.length) break;	// 배열 크기 만큼 다 돌면 break로 빠져나옴
 			}
-			if(x2-x1 > 0 && x2-x1 > max) {		
-				max = x2-x1;					
-			}									 
-												
-			start=i;							
+			if(x2-x1 > 0 && x2-x1 > max) {		// 오르막이고 max보다 크면 max에 그 값을 저장
+				max = x2-x1;			// TMI : 원래 arraylist에 저장하고 다시 최댓값 구했는데
+			}					//		 혜리 언니가 푼 방법 듣고 바로 max에 저장해서 
+								//		 max를 출력 하는 걸로 바꿨숨,,
+			start=i;				// 오르막이 아니라면 아닌 지점을 다시 출발점으로 저장
 		}	
-		System.out.println(max);
+		System.out.println(max);			// 결과 
 		
 		
 	}
