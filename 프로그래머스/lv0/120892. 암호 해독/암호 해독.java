9c2d5453eeed41cabaec65db8deb7e42571d3592
dasoLee <116339021+Losadee@users.noreply.github.com>
2023-04-16 17:23:48 +0900

    [level 0] Title: 암호 해독, Time: 2.89 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        String[] strArr = cipher.split("");
+        for(int i = code - 1; i < strArr.length; i += code) {
+        	answer += strArr[i];
+        }
+        
+        return answer;
+    }
+}
